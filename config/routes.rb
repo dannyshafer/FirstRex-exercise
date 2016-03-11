5ef106d717f841cb1c14c0f28adb4032bdbd3bb4
Daniel Shafer <github@dannyshafer.com>
2016-03-11 09:59:13 -0800

    commit before replacing httparty with rest-client

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/get_user_info' => 'base_users#get_user_info'
   get '/get_all_notes' => 'base_users#get_all_notes'
   get '/get_user_notes' => 'base_users#get_user_notes'
+  get '/post_scores' => 'base_users#post_engagement_score_to_base'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
