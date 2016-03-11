200451a01ff3b057c5f3573a1d55ba9283af384e
Daniel Shafer <github@dannyshafer.com>
2016-03-10 21:22:15 -0800

    make chart view for current month and make new class methods for previous month

--- app/models/base_user.rb
+++ app/models/base_user.rb
@@ -38,4 +38,44 @@ class BaseUser < ActiveRecord::Base
 		end
 		return BaseUser.where(current_month_engagement_score: 4...5).count, names
 	end
+
+	def self.previous_ones
+		names = []
+		BaseUser.where(previous_month_engagement_score: 0...1).each do |user|
+			names << user.name
+		end
+		return BaseUser.where(previous_month_engagement_score: 0...1).count, names 
+	end
+
+	def self.previous_twos
+		names = []
+		BaseUser.where(previous_month_engagement_score: 1...2).each do |user|
+			names << user.name
+		end
+		return BaseUser.where(previous_month_engagement_score: 1...2).count, names
+	end
+
+	def self.previous_threes
+		names = []
+		BaseUser.where(previous_month_engagement_score: 2...3).each do |user|
+			names << user.name
+		end
+		return BaseUser.where(previous_month_engagement_score: 2...3).count, names
+	end
+
+	def self.previous_fours
+		names = []
+		BaseUser.where(previous_month_engagement_score: 3...4).each do |user|
+			names << user.name
+		end
+		return BaseUser.where(previous_month_engagement_score: 3...4).count, names
+	end
+
+	def self.previous_fives
+		names = []
+		BaseUser.where(previous_month_engagement_score: 4...5).each do |user|
+			names << user.name
+		end
+		return BaseUser.where(previous_month_engagement_score: 4...5).count, names
+	end
 end
